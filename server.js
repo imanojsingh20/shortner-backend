c2a948648c53513ca1b829eff52896f47a10748f
blackhatplay <mannuall4u@gmail.com>
2021-03-10 20:48:45 +0530

    first commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res, next) => {
 
 app.use("/shorten", shortenRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
